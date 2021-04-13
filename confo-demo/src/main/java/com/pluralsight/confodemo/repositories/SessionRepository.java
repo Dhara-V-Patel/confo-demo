76cfb83f2745d6bea1acfda6fcd8f8ce973a093e
Dhara-V-Patel <46474793+Dhara-V-Patel@users.noreply.github.com>
2021-04-13 21:13:44 +0530

    Delete confo-demo/src directory

--- confo-demo/src/main/java/com/pluralsight/confodemo/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.confodemo.repositories;
-
-import com.pluralsight.confodemo.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository extends JpaRepository<Session, Long> {
-}
